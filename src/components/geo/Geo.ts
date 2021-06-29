c0413bd21cca67653100e5eb5a019f6f618e9d35
shelleyclem <shelley.diane.clem@gmail.com>
2021-06-29 08:14:33 -0400

    added weather app

--- src/components/geo/Geo.ts
+++ src/components/geo/Geo.ts
@@ -1,7 +1,13 @@
 import React, { Component, useEffect, useState } from 'react';
 
 class Geo extends Component {
-
+    const [location: Array<number>, setLocation: Array<number>] = useState('');
+    
+    useEffect(() => {
+        const success = location => {
+            
+        }
+    })
 
     render() {
         return (
--- src/components/weather/Weather.ts
+++ src/components/weather/Weather.ts
@@ -1,4 +1,40 @@
+import React, { useEffect, useState } from 'react';
 
+const Weather: any = () => {
+    const baseURL: string = 'http://api.openweathermap.org/data/2.5/weather';
+    const key: string = '5cb0082a663be375f7d12c74f060790f';
 
+    const [results, setResults] = useState({});
+
+    let lat: string;
+    let long: string;
+
+    const fetchWeather: any = (props: string) => {
+        let url: string = `${baseURL}?lat=${lat}&${long}&appid=${key}`;
+
+        fetch(url)
+        .then((res) => {
+            res.json();
+        })
+        .then((data) => {
+            setResults(data)
+        })
+        .catch((err) => console.log(err));
+    }   
+
+    useEffect(() => {
+        fetchWeather();
+    }, []);
+
+    return(
+        <div>
+            {results.weather.map((item, index) => (
+                <>
+                <h1>{item.description}</h1>
+                <h1>{results.main.temp}</h1>
+            ))}
+        </div>
+    )
+}
 
 export default Weather;
